5548fd3559aaddc70ce13c8d019dbf8d877da97f
Shayan <shayan@seepossible.com>
2025-05-27 18:21:40 +0530

    line hide and show animation

--- src/index.ts
+++ src/index.ts
@@ -683,6 +683,20 @@ async function showAnnotationDetail(viewer: ViewerApp, target: any, focusView: a
   const targetViewName = cameraViewPlugin!.camViews.find(view => view.name === target);
   hideOtherAnnotations();
   closeButton!.style.display = 'block';
+
+  viewer.scene.modelRoot.traverse(async (object: Object3D) => {
+    object.modelObject.traverse((model: Object3D) => {
+      if (model.type === "Mesh") {
+        if (model.name.includes('gem')) {
+          return;
+        }
+        if (!model.name.includes(target)) {
+          model.visible = false;
+        }
+      }
+    });
+  });
+
   focusView(targetViewName);
 
   const cameraPlugin = viewer.getPlugin(CameraViewPlugin);
